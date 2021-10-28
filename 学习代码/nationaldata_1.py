9b15689616deb63601ec964750979792edef8abd
Coupling <1360308873@qq.com>
2021-10-28 16:45:35 +0800

    国家统计局爬虫代码更改

--- 学习代码/nationaldata_1.py
+++ 学习代码/nationaldata_1.py
@@ -33,13 +33,12 @@ def getList(length):
   for i in range(length):
 
     temp = js['returndata']['datanodes'][i]['data']['strdata']
-
+    if temp =="":
+      temp="0"
     # 城乡居民收支列表中，原网站有同比增长数据为空，若直接使用eval()会报错，需要先判断
 
     if(len(temp)!=0):
-
     # eval()数字转字符串
-
         List.append(eval(temp))
 
   return List
@@ -57,26 +56,27 @@ if __name__ == '__main__':
 
 
   # 构造参数键值对，具体数值从网页结构参数中获取
-
-  key={}
-
-  key['m']='QueryData'
-
-  key['dbcode']='fsnd'
-
-  key['rowcode']='reg'
-
-  key['colcode']='sj'
-
-  key['wds']='[{"wdcode":"zb","valuecode":"A020101"}]'
-
-  key['k1']=str(getTime()) 
-
+  # 地区生产总值
+  # key={}
+  # key['m']='QueryData'
+  # key['dbcode']='fsnd'
+  # key['rowcode']='reg'
+  # key['colcode']='sj'
+  # key['wds']='[{"wdcode":"zb","valuecode":"A020101"}]'
+  # key['k1']=str(getTime()) 
+  # key['dfwds']='[{"wdcode":"sj","valuecode":"LAST10"}]'
   # "wdcode":"reg" 地区栏
 
-  # 上海 310000 
+#2013年第一季度以来各省地区生产总值
+  key={}
+  key['m']='QueryData'
+  key['dbcode']='fsjd'
+  key['rowcode']='reg'
+  key['colcode']='sj'
+  key['wds']='[{"wdcode":"zb","valuecode":"A030001"}]'
+  key['k1']=str(getTime()) 
+  key['dfwds']='[{"wdcode":"sj","valuecode":"2013A-"}]'
 
-  key['dfwds']='[{"wdcode":"sj","valuecode":"LAST10"}]'
 
   # "wdcode":"zb" 选取左侧哪个条目,"wdcode":"sj"选项框中选取"最近6季度"
 
@@ -89,49 +89,36 @@ if __name__ == '__main__':
   # ！！！verify=False，国家统计局20年下半年改用https协议,若不加该代码无法通过SSL验证
 
   r = requests.post(url, headers=headers, params=key,verify=False)
-
   # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
-
   js = json.loads(r.text)
-
-
-
-  # 得到所需数据的一维数组，利用np.array().reshape()整理为二维数组
-
   length=len(js['returndata']['datanodes'])
 
   res=getList(length)
 
-  # 总数据划分成6行的格式
-
-  array=np.array(res).reshape(len(res)//10,10)
-
-
-
-  # np.array()转换成pd.DataFrame格式，后续可使用to_excel()直接写入excel表格
-
-  df_shanghai=pd.DataFrame(array)
   list_2 = js['returndata']['wdnodes'][2]['nodes']
   index_list = []
   for i_2 in range(len(list_2)):
     index = list_2[i_2]['cname']
     index_list.append(index)
-    print(index_list)
+  print(index_list)
   print('*'*20+'\n')
+
 # 获取列名称
   list_3 = js['returndata']['wdnodes'][1]['nodes']
   columns_list = []
   for i_2 in range(len(list_3)):
     columns = list_3[i_2]['cname']
     columns_list.append(columns)
-  df_shanghai.columns=index_list
+    len(list_3)
+  print(columns_list)
+  array=np.array(res).reshape(len(list_3),len(list_2))
+  # np.array()转换成pd.DataFrame格式，后续可使用to_excel()直接写入excel表格
+  df=pd.DataFrame(array)
+  df.index=columns_list
+  df.columns=index_list
 
-  df_shanghai.index=columns_list
 
-
-
-  print(df_shanghai)
-  write = pd.ExcelWriter('./地区生产总值.xls') #该路径自己设置即可，没有该文件的话会自行创建一个，存在的话写入会覆盖原内容
-  df_shanghai.to_excel(write)
-    #如果爬多个省份的数据，可以写入多个工作表，且必须要加上save()保存
+  print(df)
+  write = pd.ExcelWriter('./2013年第一季度居民人均可支配收入(元).xls') #该路径自己设置即可，没有该文件的话会自行创建一个，存在的话写入会覆盖原内容
+  df.to_excel(write)
   write.save()
