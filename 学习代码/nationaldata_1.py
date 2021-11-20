1826cfb02c1ae556f8389a266f6f952298e272d7
Coupling <1360308873@qq.com>
2021-11-20 09:30:33 +0800

    Update nationaldata_1.py

--- 学习代码/nationaldata_1.py
+++ 学习代码/nationaldata_1.py
@@ -1,124 +1,80 @@
-import urllib3 
-
-# 使用urllib3.disable_warnings()在关闭SSL认证（verify=False）情况下
-
-# 将requests请求禁用安全请求警告
-
-import requests    # 使用Requests发送网络请求
-
-import time      # 用来获取时间戳(计算当前时间，用于网页验证)
-
-import json      # 处理json文件
-
-import numpy as np  # 处理数组
-
-import pandas as pd  # np.array()转换成pd.DataFrame格式，再使用to_excel()写入excel表格
+import urllib.request
+import requests
+import time
+import json
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup  # 导入urllib库的request模块
+import lxml                    #文档解析器
+import os                      #os模块就是对操作系统进行操作
 
 
 
-# 获取毫秒级时间戳，用于网页验证
 
-def getTime():
-
-  return int(round(time.time() * 1000))
+def get(url):
 
 
+    # 禁用安全请求警告
+    requests.packages.urllib3.disable_warnings()
+
+    # 发出请求，使用post方法，这里使用前面自定义的头部和参数
+    # verify=False，国家统计局20年下半年改用https协议,若不加该代码无法通过SSL验证
+    r = requests.post(url, headers=headers,verify=False)
+
+    # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
+    js = json.loads(r.text)
+    # print(js)
 
 # 数据预处理，获取json列表中层层包裹的strdata元素（数据）
+    def getList(length):
+        Titles=[]
+        urls=[]
+        for i in range(length):
+            Title = js['data']['resultList'][i]['dreTitle']
+            time = js['data']['resultList'][i]['docDate']
+            organization = js['data']['resultList'][i]['domainSiteName']
+            suburl = js['data']['resultList'][i]['url']
+            print(Title+time+organization+suburl)
 
-def getList(length):
+            Titles.append(Title)
+            urls.append(suburl)
 
-  List=[]
+        return Titles,urls
 
-  for i in range(length):
 
-    temp = js['returndata']['datanodes'][i]['data']['strdata']
-    if temp =="":
-      temp="0"
-    # 城乡居民收支列表中，原网站有同比增长数据为空，若直接使用eval()会报错，需要先判断
+    length=len(js['data']['resultList'])
+    Titles,urls=getList(length)
 
-    if(len(temp)!=0):
-    # eval()数字转字符串
-        List.append(eval(temp))
 
-  return List
 
+
+    for i in range(len(urls)):
+        try: 
+            res = urllib.request.urlopen(urls[i])  
+            html = res.read().decode('utf-8') 
+            soup = BeautifulSoup(html, 'lxml')
+
+            print(str(i)+'saved')
+            for p in soup.select('p'):
+                t = p.get_text()
+                txt(Titles[i],t)
+        except OSError:
+            pass    #如果报错就不管，继续读取下一个url
+        continue
+    
+#定义txt存储路径。
+picpath='./newws/'#这里我用的是本程序路径，也可改为c盘或d盘等路径。
+def txt(name, text):  # 定义函数名
+    if not os.path.exists(picpath):  # 路径不存在时创建一个
+        os.makedirs(picpath)
+    savepath = picpath + name + '.txt'
+    file = open(savepath, 'a', encoding='utf-8')#因为一个网页里有多个标签p，所以用'a'添加模式
+    file.write(text)
+    # print(text)
+    file.close
 if __name__ == '__main__':
+    for i in range(5):
+        url='https://fwfx.ndrc.gov.cn/api/query?qt=&tab=all&page='+str(i)+'&pageSize=20&siteCode=bm04000fgk&key=CAB549A94CF659904A7D6B0E8FC8A7E9&startDateStr=&endDateStr=&timeOption=0&sort=dateDesc'
+        headers={'User-Agent':'Mozilla/5.0(Windows;U;Windows NT6.1;en-US;rv:1.9.1.6) Geko/20091201 Firefox/3.5.6'}#浏览器代理
+        get(url)
 
-  # 请求目标网址(链接?前面的东西)
-
-  url='https://data.stats.gov.cn/easyquery.htm'
-
-  # 请求头，User-Agent: 用来证明你是浏览器，满足一定格式即可，不一定和自己的浏览器一样
-
-  headers={'User-Agent':'Mozilla/5.0(Windows;U;Windows NT6.1;en-US;rv:1.9.1.6) Geko/20091201 Firefox/3.5.6'}#浏览器代理
-
-
-
-  # 构造参数键值对，具体数值从网页结构参数中获取
-  # 地区生产总值
-  # key={}
-  # key['m']='QueryData'
-  # key['dbcode']='fsnd'
-  # key['rowcode']='reg'
-  # key['colcode']='sj'
-  # key['wds']='[{"wdcode":"zb","valuecode":"A020101"}]'
-  # key['k1']=str(getTime()) 
-  # key['dfwds']='[{"wdcode":"sj","valuecode":"LAST10"}]'
-  # "wdcode":"reg" 地区栏
-
-#2013年第一季度以来各省地区生产总值
-  key={}
-  key['m']='QueryData'
-  key['dbcode']='fsnd'
-  key['rowcode']='reg'
-  key['colcode']='sj'
-  key['wds']='[{"wdcode":"zb","valuecode":"A0G0701"}]'
-  key['k1']=str(getTime()) 
-  key['dfwds']='[{"wdcode":"sj","valuecode":"1996-"}]'
-
-
-  # "wdcode":"zb" 选取左侧哪个条目,"wdcode":"sj"选项框中选取"最近6季度"
-
-  # 禁用安全请求警告
-
-  requests.packages.urllib3.disable_warnings()
-
-  # 发出请求，使用post方法，这里使用前面自定义的头部和参数
-
-  # ！！！verify=False，国家统计局20年下半年改用https协议,若不加该代码无法通过SSL验证
-
-  r = requests.post(url, headers=headers, params=key,verify=False)
-  # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
-  js = json.loads(r.text)
-  length=len(js['returndata']['datanodes'])
-
-  res=getList(length)
-
-  list_2 = js['returndata']['wdnodes'][2]['nodes']
-  index_list = []
-  for i_2 in range(len(list_2)):
-    index = list_2[i_2]['cname']
-    index_list.append(index)
-  print(index_list)
-  print('*'*20+'\n')
-
-# 获取列名称
-  list_3 = js['returndata']['wdnodes'][1]['nodes']
-  columns_list = []
-  for i_2 in range(len(list_3)):
-    columns = list_3[i_2]['cname']
-    columns_list.append(columns)
-    len(list_3)
-  print(columns_list)
-  array=np.array(res).reshape(len(list_3),len(list_2))
-  # np.array()转换成pd.DataFrame格式，后续可使用to_excel()直接写入excel表格
-  df=pd.DataFrame(array)
-  df.index=columns_list
-  df.columns=index_list
-
-
-  print(df)
-  write = pd.ExcelWriter('./1996~2020民用汽车拥有量（万辆）.xls') #该路径自己设置即可，没有该文件的话会自行创建一个，存在的话写入会覆盖原内容
-  df.to_excel(write)
-  write.save()
