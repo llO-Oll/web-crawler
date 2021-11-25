e95e408dbb603352d94e199385e0eaec56725395
Coupling <1360308873@qq.com>
2021-11-25 16:26:08 +0800

    update

--- 学习代码/gjfgw.py
+++ 学习代码/gjfgw.py
@@ -9,11 +9,8 @@ from bs4 import BeautifulSoup  # 导入urllib库的request模块
 import lxml                    #文档解析器
 import os                      #os模块就是对操作系统进行操作
 
-
-
-num =0
-def get(url):
-
+count = 0
+def get(url,headers):
 
     # 禁用安全请求警告
     requests.packages.urllib3.disable_warnings()
@@ -24,31 +21,28 @@ def get(url):
 
     # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
     js = json.loads(r.text)
-    # print(js)
-
-# 数据预处理，获取json列表中层层包裹的strdata元素（数据）
-    def getList(length):
-        Titles=[]
-        urls=[]
-        times=[]
-        for i in range(length):
-            Title = js['data']['resultList'][i]['dreTitle']
-            time = js['data']['resultList'][i]['docDate']
-            organization = js['data']['resultList'][i]['domainSiteName']
-            suburl = js['data']['resultList'][i]['url']
-            print(Title+time+organization+suburl)
-            if 'pdf' not in suburl:
-                Titles.append(Title)
-                urls.append(suburl)
-                times.append(time)
-        return Titles,urls,times
-
 
     length=len(js['data']['resultList'])
-    Titles,urls,times=getList(length)
+    return js,length
 
-
-
+# 分析json
+def getList(js,length):
+    Titles=[]
+    urls=[]
+    times=[]
+    global count
+    for i in range(length):
+        Title = js['data']['resultList'][i]['dreTitle']
+        time = js['data']['resultList'][i]['docDate']
+        organization = js['data']['resultList'][i]['domainSiteName']
+        suburl = js['data']['resultList'][i]['url']
+        count=count+1
+        print(str(count)+Title+time+organization+suburl)
+        if 'pdf' not in suburl:
+            Titles.append(Title)
+            urls.append(suburl)
+            times.append(time)
+    # return Titles,urls,times
 
     for i in range(len(urls)):
         try: 
@@ -56,7 +50,7 @@ def get(url):
             html = res.read().decode('utf-8') 
             soup = BeautifulSoup(html, 'lxml')
 
-            print(str(i)+'saved')
+            
             t=''
             for p in soup.select('p'):
                 temp = p.get_text()
@@ -65,23 +59,28 @@ def get(url):
         except OSError:
             pass    #如果报错就不管，继续读取下一个url
         continue
-#定义txt存储路径。
-picpath='./syq/'#这里我用的是本程序路径，也可改为c盘或d盘等路径。
+num=0
+picpath='F:/xlx/'
 def txt(name,time, text):  # 定义函数名
     global num
     if not os.path.exists(picpath):  # 路径不存在时创建一个
         os.makedirs(picpath)
-    savepath = picpath+'国家发改委 '+time +' '+name+ '.txt'
+    savepath = picpath+str(num)+ '.txt'
     num=num+1
     file = open(savepath, 'a', encoding='utf-8')#因为一个网页里有多个标签p，所以用'a'添加模式
     file.write(name)
-    file.write('\r\n')
+    file.write('\r')
+    file.write(time)
+    file.write('\r')
+    file.write('国家发改委')
+    file.write('\r')
     file.write(text)
     # print(text)
     file.close
+
 if __name__ == '__main__':
-    for i in range(40):
+    for i in range(25):
         url='https://fwfx.ndrc.gov.cn/api/query?qt=%E7%94%B5%E5%8A%9B&tab=all&page='+str(i)+'&pageSize=20&siteCode=bm04000fgk&key=CAB549A94CF659904A7D6B0E8FC8A7E9&startDateStr=&endDateStr=&timeOption=0&sort=dateDesc'
         headers={'User-Agent':'Mozilla/5.0(Windows;U;Windows NT6.1;en-US;rv:1.9.1.6) Geko/20091201 Firefox/3.5.6'}#浏览器代理
-        get(url)
-
+        js,length=get(url,headers)
+        getList(js,length)
