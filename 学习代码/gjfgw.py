743e4d3f9edd0f9eb4f937d9847ed4c3d7825f62
Coupling <57003610+llO-Oll@users.noreply.github.com>
2021-12-01 12:00:34 +0800

    update

--- 学习代码/gjfgw.py
+++ 学习代码/gjfgw.py
@@ -4,24 +4,20 @@ import time
 import json
 import numpy as np
 import pandas as pd
-from bs4 import BeautifulSoup  # 导入urllib库的request模块
-import lxml                    #文档解析器
-import os                      #os模块就是对操作系统进行操作
+from bs4 import BeautifulSoup  
+import lxml                    
+import os                      
 
 import re
 count = 0
-def get(url,headers):
-
-    # 禁用安全请求警告
-    requests.packages.urllib3.disable_warnings()
+def get(url,headers):   
+    requests.packages.urllib3.disable_warnings()    # 禁用安全请求警告
 
     # 发出请求，使用post方法，这里使用前面自定义的头部和参数
-    # verify=False，国家统计局20年下半年改用https协议,若不加该代码无法通过SSL验证
+    # verify=False,https协议,若不加该代码无法通过SSL验证
     r = requests.post(url, headers=headers,verify=False)
 
-    # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
-    js = json.loads(r.text)
-
+    js = json.loads(r.text)    # 使用json库中loads函数，将r.text字符串解析成dict字典格式存储于js中
     length=len(js['data']['resultList'])
     return js,length
 
@@ -30,97 +26,107 @@ def getList(js,length):
     Titles=[]
     urls=[]
     times=[]
-    zctypes=[]
+    fujianurl=[]
     global count
+
+    #从一级网址获取信息
     for i in range(length):
-        Title = js['data']['resultList'][i]['dreTitle']
-        time = js['data']['resultList'][i]['docDate']
-        organization = js['data']['resultList'][i]['domainSiteName']
-        suburl = js['data']['resultList'][i]['url']
-        count=count+1
-        print(str(count)+Title+time+organization+suburl)
-        if 'pdf' not in suburl and 'zcfb' in suburl:
+        Title = js['data']['resultList'][i]['dreTitle']                 #政策标题
+        time = js['data']['resultList'][i]['docDate']                   #政策发布时间
+        organization = js['data']['resultList'][i]['domainSiteName']    #机构
+        suburl = js['data']['resultList'][i]['url']                     #二级网址    
+
+        if 'pdf' not in suburl and 'zcfb' in suburl:        # 判断二级网址是否是pdf，如果不是将政策信息添加到list中
+            count=count+1
+            Title=Title.replace('<em>','')
+            Title=Title.replace('</em>','')    #剔除由关键字搜索产生的'<em>关键词</em>'   
             Titles.append(Title)
             urls.append(suburl)
             times.append(time)
-    # return Titles,urls,times
+            print(str(count)+Title+time+organization+suburl)
 
+    #爬取二级url
     for i in range(len(urls)):
         try: 
             res = urllib.request.urlopen(urls[i])  
             html = res.read().decode('utf-8') 
-            soup = BeautifulSoup(html, 'lxml')
-
-            
+            soup = BeautifulSoup(html, 'lxml')            
             t=''
-            for p in soup.select('p'):
-                temp = p.get_text()
-                t =t+temp
-            txt(Titles[i],times[i],t)
-            fatherurl =re.match('https://www.ndrc.gov.cn/xxgk/zcfb/(.*)/',urls[i] )
+            fatherurl =re.match('https://www.ndrc.gov.cn/xxgk/zcfb/(.*)/',urls[i] )     #匹配根网址，附件url=根+子级url
             if 'gg' in fatherurl.group():
                 for p in soup.select('p'):
                     temp = p.get_text()
                     t =t+'\r'+temp
                 txt(Titles[i],times[i],t)
             else:
-                soup.find_all('')
                 for p in soup.select('span'):
                     temp = p.get_text()
                     t =t+'\r'+temp
                 txt(Titles[i],times[i],t)
+            
+
+            #是否存在pdf附件,存在获取url并下载
+            for a in soup.find_all(href=re.compile(".pdf")):
+                temp_url=a.get('href')                  # 获取子级url
+                temp_url=temp_url.replace('./','')      # 替换子级url前面的'./'
+                print(temp_url)
+                filename=a.get_text()+'.pdf'
+                url=fatherurl.group()+temp_url          # 完整的附件url
+                print(url)
+                # getFile(url,filename)
+                # print('sucess download'+filename)
+
         except OSError:
             pass    #如果报错就不管，继续读取下一个url
         continue
+
+#保存政策txt
 num=0
-picpath='F:/xlx/'
-def txt(name,time, text):  # 定义函数名
+filepath='F:/policies/'     # 政策保存路径
+def txt(name,time, text):  
     global num
-    if not os.path.exists(picpath):  # 路径不存在时创建一个
-        os.makedirs(picpath)
-    savepath = picpath+str(num)+ '.txt'
+    if not os.path.exists(filepath):  # 路径不存在时创建一个
+        os.makedirs(filepath)
+    savepath = filepath+str(num)+ '.txt'
     num=num+1
-    file = open(savepath, 'a', encoding='utf-8')#因为一个网页里有多个标签p，所以用'a'添加模式
+    file = open(savepath, 'a', encoding='utf-8')    #因为一个网页里有多个标签p或者span，所以用'a'添加模式
+    
     file.write(name)
     file.write('\r')
     file.write(time)
     file.write('\r')
     file.write('国家发改委')
     file.write('\r')
-    # text=text.replace('  ','\r')
-    file.write(text)
+    text=text.replace('<em>','')
+    text=text.replace('</em>','')               #剔除由关键字搜索产生的'<em>关键词</em>'
+    file.write(text)                            #保存正文
     # print(text)
     file.close
 
-# class Policies():
-#     def Policytz(self):
-#         f = fatherurl+'tz'
-#         return f
-#     def Policygh(self):
-#         f = fatherurl+'ghwb'
-#         return f
-#     def Policygf(self):
-#         f = fatherurl+'ghxwj'
-#         return f
-#     def Policygg(self):
-#         f = fatherurl+'gg'
-#         return f    
-#     def Policyfz(self):
-#         f = fatherurl+'fzggwl'
-#         return f
-#     # def Default(self):
-#     #     return "Invalid type"
-#     def getPolicy(self, policy):
+#下载pdf
+# def getFile(url,file_name):
+#     try:
+#         u = urllib.request.urlopen(url)
+#     except urllib.error.HTTPError:
+#        #碰到了匹配但不存在的文件时，提示并返回
+#         print(url, "url file not found")
+#         return
+#     if not os.path.exists(filepath):  # 路径不存在时创建一个
+#         os.makedirs(filepath)
+#     block_sz = 8192
+#     with open(filepath+file_name, 'wb') as f:
+#         while True:
+#             buffer = u.read(block_sz)
+#             if buffer:
+#                 f.write(buffer)
+#             else:
+#                 break
+#     print ("Sucessful to download" + " " + file_name)
 
-
-#         policy_name = "Policy" + str(policy)
-
-#         fun = getattr(self, policy_name, self.Default)
-#         return fun()
 
 if __name__ == '__main__':
     for i in range(25):
-        url='https://fwfx.ndrc.gov.cn/api/query?qt=%E7%94%B5%E5%8A%9B&tab=all&page='+str(i)+'&pageSize=20&siteCode=bm04000fgk&key=CAB549A94CF659904A7D6B0E8FC8A7E9&startDateStr=&endDateStr=&timeOption=0&sort=dateDesc'
+        url='https://fwfx.ndrc.gov.cn/api/query?qt=%E7%94%B5%E5%8A%9B&tab=all&page='+str(i)+'&pageSize=20&siteCode=bm04000fgk&key=CAB549A94CF659904A7D6B0E8FC8A7E9&startDateStr=&endDateStr=&timeOption=0&sort=weight'
         headers={'User-Agent':'Mozilla/5.0(Windows;U;Windows NT6.1;en-US;rv:1.9.1.6) Geko/20091201 Firefox/3.5.6'}#浏览器代理
         js,length=get(url,headers)
         getList(js,length)
--- 学习代码/test.py
+++ 学习代码/test.py
@@ -1,23 +1,68 @@
-from bs4 import BeautifulSoup
+import urllib.request
 import requests
+import time
+import json
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup  # 导入urllib库的request模块
+import lxml                    #文档解析器
+import os                      #os模块就是对操作系统进行操作
+import re
 # 页面url地址
-url = 'http://newgame.17173.com/game-list-0-0-0-0-0-0-0-0-0-0-1-2.html'
 
-# 发送请求，r为页面响应
-r = requests.get(url)
 
-# r.text获取页面代码
-# 使用lxml解析页面代码
-soup = BeautifulSoup(r.text, 'lxml')
+url = 'https://www.ndrc.gov.cn/xxgk/zcfb/fzggwl/202009/t20200923_1239477.html'
+fatherurl =re.match('https://www.ndrc.gov.cn/xxgk/zcfb/(.*)/',url)
+res = urllib.request.urlopen(url)  
+html = res.read().decode('utf-8') 
+soup = BeautifulSoup(html, 'lxml')    
+t=''
 
-# 两次定位，先找到整个信息区域
-info_list = soup.find_all(attrs={'class': 'ptlist ptlist-pc'})
 
-# 在此区域内获取游戏名，find_all返回的是list
-tit_list = info_list[0].find_all(attrs={'class': 'tag'})
+picpath='F:/test/'
+def txt(name,time, text):  # 定义函数名
+    if not os.path.exists(picpath):  # 路径不存在时创建一个
+        os.makedirs(picpath)
+    savepath = picpath+ 'test.txt'
+    file = open(savepath, 'a', encoding='utf-8')#因为一个网页里有多个标签p，所以用'a'添加模式
+    file.write(name)
+    file.write('\r')
+    file.write(time)
+    file.write('\r')
+    file.write('国家发改委')
+    file.write('\r')
+    # text=text.replace('  ','\r')
+    file.write(text)
+    # print(text)
+    file.close
+txt('TEST','2021',t)
 
-# 遍历获取游戏名
-# .text可获取文本内容，替换掉文章中的换行符
-for title in tit_list:
-    # print(title.text.replace('\n', ''))
-    print(title.text)
+def getFile(url,file_name):
+    try:
+        u = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+       #碰到了匹配但不存在的文件时，提示并返回
+        print(url, "url file not found")
+        return
+    if not os.path.exists(picpath):  # 路径不存在时创建一个
+        os.makedirs(picpath)
+    block_sz = 8192
+    with open(picpath+file_name+'.pdf', 'wb') as f:
+        while True:
+            buffer = u.read(block_sz)
+            if buffer:
+                f.write(buffer)
+            else:
+                break
+for p in soup.select('span'):
+    temp = p.get_text()
+    t =t+'\r'+temp
+for a in soup.find_all(href=re.compile(".pdf")):
+    temp_url=a.get('href')
+    temp_url=temp_url.replace('./','')
+    print(temp_url)
+    filename=a.get_text()
+    url=fatherurl.group()+temp_url
+    print(url)
+    getFile(url,filename)
+
